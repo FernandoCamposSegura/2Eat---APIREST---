403fea150bbe3e8efbbf042d6e405700d8607f22
FernandoCamposSegura <campossegura19@gmail.com>
2023-03-08 17:50:20 +0100

    Init

---
+++ src/main/java/com/svalero/toeat/ToeatApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.toeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/svalero/toeat/ToeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svalero.toeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
